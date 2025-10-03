05ae3587f9582b73f533788fdde24f2076443ea4
hqb2k4 <hoangquocbao4102k4@gmail.com>
2025-10-03 20:43:46 +0700

    String type

---
+++ stringtype/stringtype.js
@@ -0,0 +1,7 @@
+// ts assign string 
+var a = "Dep trai";
+var b = "Hoang Quoc Bao";
+var c = "".concat(b, " ").concat(a, " vo cung");
+console.log("He He:", c);
+console.log("Ai cung" + a);
+console.log(c);
---
+++ stringtype/stringtype.ts
@@ -0,0 +1,10 @@
+// ts assign string 
+let a = "Dep trai";
+
+let b: string = "Hoang Quoc Bao";
+
+const c:string = `${b} ${a} vo cung`;
+
+console.log("He He:", c);
+console.log("Ai cung" + a);
+console.log(c);
