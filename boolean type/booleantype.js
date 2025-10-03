3691ec3145bd1959ad040650d0d22316130d1d2d
hqb2k4 <hoangquocbao4102k4@gmail.com>
2025-10-03 20:49:35 +0700

    boolean type

---
+++ boolean type/booleantype.js
@@ -0,0 +1,11 @@
+// ts assign boolean
+var isDone = false;
+// ts annotate boolean
+var isOk = false;
+var a = 5;
+if (a > 3) {
+    isDone = true;
+    if (isDone) {
+        console.log("a is greater than 3");
+    }
+}
---
+++ boolean type/booleantype.ts
@@ -0,0 +1,13 @@
+// ts assign boolean
+let isDone = false;
+
+// ts annotate boolean
+const isOk: boolean = false;
+
+let a = 5;
+if (a > 3) {
+  isDone = true;
+  if (isDone) {
+    console.log("a is greater than 3");
+  }
+}
